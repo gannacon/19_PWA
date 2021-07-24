e33f1dbc2908da07db473f95e41ce280714af598
gannacon <cwgannaway@gmail.com>
2021-07-24 00:17:56 -0500

    added db file to handle indexeddb and working

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,9 +21,9 @@ self.addEventListener("install", function (evt) {
   );
 
   // // pre cache all static assets
-  // evt.waitUntil(
-  //   caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  // );
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
 
   // tell the browser to activate this service worker immediately once it
   // has finished installing
