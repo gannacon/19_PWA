73b25fd97f46099f157d36f4c268b9fe12ce1ad0
gannacon <cwgannaway@gmail.com>
2021-07-28 23:31:46 -0600

    work on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -66,7 +66,7 @@ self.addEventListener("fetch", function (evt) {
 });
 
 self.addEventListener("fetch", function (evt) {
-  if (evt.request.url.includes("/api/")) {
+  if (evt.request.url.includes("/api/transaction")) {
     console.log("[Service Worker] Fetch (data)", evt.request.url);
 
     evt.respondWith(
